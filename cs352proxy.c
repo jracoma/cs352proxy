fa8a5e4fffc1b1a99d1627c22fae1112d5c12832
jracoma <jshinra@hotmail.com>
2013-12-01 13:06:39 -0500

    Working zero server and first client connect

--- cs352proxy.c
+++ cs352proxy.c
@@ -322,8 +322,7 @@
  	char *buffer = malloc(MAXBUFFSIZE);
  	struct peerList *peer = (struct peerList *)temp;
  	struct peerList *newPeer = malloc(sizeof(struct peerList));
- 	struct linkState test;
- 	struct linkStateSource *lsSource = (struct linkStateSource *)malloc(sizeof(struct linkStateSource));
+ 	struct linkStateSource lsSource;
  	struct linkStatePacket *lsPacket = (struct linkStatePacket *)malloc(sizeof(struct linkStatePacket));
  	uint16_t pHeaderInfo[2];
 
@@ -357,9 +356,9 @@
  			pthread_mutex_lock(&peer_mutex);
  			LL_APPEND(peerHead, peer);
  			pthread_mutex_unlock(&peer_mutex);
- 			test.listenPort = ntohs(local_info->listenPort);
 
- 			lsSource->ls = &temp;
+
+ 			lsSource->ls = &local_info;
  			// memcpy(lsSource->ls, &local_info, sizeof(local_info));
  			LL_COUNT(peerHead, peer, lsSource->neighbors);
  			printf("Test: %s, %d\n", (char *)local_info, lsSource->neighbors);
