b189adc0487227545803926b2bf8bb83ee5f8f8e
jracoma <jshinra@hotmail.com>
2013-12-01 12:37:28 -0500

    Working zero server and first client connect

--- cs352proxy.c
+++ cs352proxy.c
@@ -357,7 +357,7 @@
  			LL_APPEND(peerHead, peer);
  			pthread_mutex_unlock(&peer_mutex);
  			// lsSource->ls = (struct linkState *)&local_info;
- 			memcpy(lsSource->, local_info, sizeof(local_info));
+ 			memcpy(lsSource->ls, local_info, sizeof(local_info));
  			LL_COUNT(peerHead, peer, lsSource->neighbors);
  			printf("Test: %s, %d\n", (char *)local_info, lsSource->neighbors);
  		}
