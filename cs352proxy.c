cb1819cc94e6e7a8b167659d5587f971f94702d3
jracoma <jshinra@hotmail.com>
2013-11-30 20:57:35 -0500

    Working zero server and first client connect

--- cs352proxy.c
+++ cs352proxy.c
@@ -98,7 +98,7 @@ int initLocalParams() {
   	perror("ioctl(SIOCGIFADDR");
   	return EXIT_FAILURE;
   }
-  local_info->ethMAC = ifr.ifr_hwaddr;
+  local_info->ethMAC = &ifr->ifr_hwaddr;
 
   sprintf(ethMAC, " %02x:%02x:%02x\n", (unsigned char)ifr.ifr_hwaddr.sa_data[0], (unsigned char)ifr.ifr_hwaddr.sa_data[1], (unsigned char)ifr.ifr_hwaddr.sa_data[2]);
   // sprintf(buffer, "/sys/class/net/%s/address", dev);
