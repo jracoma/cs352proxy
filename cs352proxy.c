904ca5c48d9ac89b6cbe3cbad8a7d0395be41a27
jracoma <jshinra@hotmail.com>
2013-12-01 01:09:03 -0500

    Working zero server and first client connect

--- cs352proxy.c
+++ cs352proxy.c
@@ -176,10 +176,12 @@ int parseInput(int argc, char *argv[]) {
 			current->peerPort = port;
 			current->tapDevice = (char *)malloc(50);
 			strcpy(current->tapDevice, tapDevice);
+			pthread_mutex_lock(&peer_mutex);
 			if (pthread_create(&connect_thread, NULL, connectToPeer, (void *)current) != 0) {
 			  perror("connect_thread");
 			  exit(1);
 			}
+			pthread_mutex_unlock(&peer_mutex);
 		}
 	}
 
