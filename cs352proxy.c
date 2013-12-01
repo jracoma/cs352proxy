2022b737a7037d879efd411205319f02bc96b83a
jracoma <jshinra@hotmail.com>
2013-12-01 00:18:41 -0500

    Working zero server and first client connect

--- cs352proxy.c
+++ cs352proxy.c
@@ -188,7 +188,7 @@ int parseInput(int argc, char *argv[]) {
 				inet_aton(host, &newPeer->peerIP);
 				newPeer->peerPort = port;
 				newPeer->tapDevice = tapDevice;
-				if (!(client_fd = connectToPeer(newPeer))) {
+				if ((client_fd = connectToPeer(newPeer))) {
 					printf("Peer Removed %s:%d: Failed to connect\n", inet_ntoa(newPeer->peerIP), newPeer->peerPort);
 				} else {
 					printf("Peer Added %s:%d: Successful connection\n", inet_ntoa(newPeer->peerIP), newPeer->peerPort);
