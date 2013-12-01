fe69d095fd95defb32544bbeff34ebebaec52dff
jracoma <jshinra@hotmail.com>
2013-11-30 23:52:24 -0500

    Working zero server and first client connect

--- cs352proxy.c
+++ cs352proxy.c
@@ -105,7 +105,7 @@ int initLocalParams() {
 	FILE *f = fopen(buffer, "r");
 	fread(buffer, 1, MAXLINESIZE, f);
 	// sscanf(buffer ,"%hhX:%hhX:%hhX:%hhX:%hhX:%hhX", (unsigned char *)ethMAC[0], (unsigned char *)ethMAC[0], (unsigned char *)ethMAC[0], (unsigned char *)ethMAC[0], (unsigned char *)ethMAC[0], (unsigned char *)ethMAC[0]);
-  printf("Buffer: %s\n", buffer)
+  printf("Buffer: %s\n", buffer);
   fclose(f);
 
 	if (debug) {
