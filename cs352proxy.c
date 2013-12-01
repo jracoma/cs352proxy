1e1f7c2e9fc805d9e00fba6fe2e19ff10055a440
jracoma <jshinra@hotmail.com>
2013-11-30 23:09:13 -0500

    Working zero server and first client connect

--- cs352proxy.c
+++ cs352proxy.c
@@ -76,9 +76,9 @@ int getIP(char *host, char *ip) {
 int initLocalParams() {
   struct ifreq ifr;
   char ethMAC[19];
-  struct local_info = malloc(sizeof(struct linkState));
+  local_info = malloc(sizeof(struct linkState));
 
-        sock_fd = socket(AF_INET, SOCK_DGRAM, 0);
+  sock_fd = socket(AF_INET, SOCK_DGRAM, 0);
 
     //Type of address to retrieve - IPv4 IP address
     ifr.ifr_addr.sa_family = AF_INET;
