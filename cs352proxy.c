b20909352b9d86d105cbc0b7b8e1465f5bc4b510
jracoma <jshinra@hotmail.com>
2013-12-01 01:25:11 -0500

    Working zero server and first client connect

--- cs352proxy.c
+++ cs352proxy.c
@@ -341,7 +341,7 @@
     /* Connect to server */
     if ((connect(new_fd, (struct sockaddr *)&remote_addr, sizeof(remote_addr))) != 0) {
     	pthread_mutex_unlock(&peer_mutex);
-      pthread_exit(-1);
+      return -1;
     } else {
 	    printf("Connected to server %s:%d\n", inet_ntoa(remote_addr.sin_addr), htons(remote_addr.sin_port));
       char *message = "The Cheese is in The Toaster";
