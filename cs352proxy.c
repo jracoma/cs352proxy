1c48e0442202428ba2e4d1ce71925e0af29a2eb1
jracoma <jshinra@hotmail.com>
2013-12-02 00:12:27 -0500

    Working zero server and first client connect

--- cs352proxy.c
+++ cs352proxy.c
@@ -392,7 +392,7 @@
  		sin->sin_family = ARPHRD_ETHER;
  		char *tapDevice;
  		// strcpy(peer->tapDevice, tapDevice);
- 		printf("TEST TEST %s\n", &(peer->tapDevice));
+ 		printf("TEST TEST %s\n", &(peer->(&tapDevice));
 		// strncpy(areq.arp_dev, peer->(&tapDevice), 15);
 
 	if (ioctl(sock_fd, SIOCGARP, (caddr_t) &areq) == -1) {
