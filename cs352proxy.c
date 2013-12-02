a8417799a037f33db77d6379b1fd9972aa9485d4
jracoma <jshinra@hotmail.com>
2013-12-01 19:35:13 -0500

    Working zero server and first client connect

--- cs352proxy.c
+++ cs352proxy.c
@@ -390,8 +390,8 @@ int send_linkStatePacket(struct linkStatePacket *lsp) {
 
  	/* Serialize data */
 	lsp->header->length = sizeof(lsp);
-	sprintf(buffer, "%x %x %s %d %02x:%02x:%02x:%02x:%02x:%02x", lsp->header->type, lsp->header->length, inet_ntoa(lsp->source->ls->listenIP), ntohs(lsp->source->ls->listenPort), (unsigned char)lsp->source->ls->ethMAC.sa_data[0], (unsigned char)lsp->source->ls->ethMAC.sa_data[1], (unsigned char)lsp->source->ls->ethMAC.sa_data[2], (unsigned char)lsp->source->ls->ethMAC.sa_data[3], (unsigned char)lsp->source->ls->ethMAC.sa_data[4], (unsigned char)lsp->source->ls->ethMAC.sa_data[5]);
-	send(peer->net_fd, &buffer, strlen(&buffer), 0);
+	// sprintf(buffer, "%x %x %s %d %02x:%02x:%02x:%02x:%02x:%02x", lsp->header->type, lsp->header->length, inet_ntoa(lsp->source->ls->listenIP), ntohs(lsp->source->ls->listenPort), (unsigned char)lsp->source->ls->ethMAC.sa_data[0], (unsigned char)lsp->source->ls->ethMAC.sa_data[1], (unsigned char)lsp->source->ls->ethMAC.sa_data[2], (unsigned char)lsp->source->ls->ethMAC.sa_data[3], (unsigned char)lsp->source->ls->ethMAC.sa_data[4], (unsigned char)lsp->source->ls->ethMAC.sa_data[5]);
+	send(peer->net_fd, lsp->header, strlen(lsp->header), 0);
 
 	pthread_mutex_unlock(&peer_mutex);
 	pthread_mutex_unlock(&linkstate_mutex);
