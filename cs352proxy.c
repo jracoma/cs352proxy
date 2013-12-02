0b614c443281b14e6b8313e068a1c813b7eacb86
jracoma <jshinra@hotmail.com>
2013-12-02 03:11:41 -0500

    Working zero server and first client connect

--- cs352proxy.c
+++ cs352proxy.c
@@ -409,7 +409,7 @@
  }
 
 /* Send linkState */
- void send_linkState(struct linkState *ls, int peer_fd) {
+ char send_linkState(struct linkState *ls, int peer_fd) {
  	char buffer[MAXBUFFSIZE];
 
  	/* Serialize Data - Packet Type | listenIP | listenPort | ethMAC */
