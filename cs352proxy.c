6ccd7f936c4fe28a5d7b0d74f3ed1a004d1fa746
jracoma <jshinra@hotmail.com>
2013-12-02 00:20:41 -0500

    Working zero server and first client connect

--- cs352proxy.c
+++ cs352proxy.c
@@ -170,10 +170,7 @@
  			current = malloc(sizeof(struct peerList));
  			inet_aton(host, &current->peerIP);
  			current->peerPort = port;
- 			// current->tapDevice = (char *)malloc(50);
- 			*current->tapDevice = tapDevice;
  			printf("TESTSTST: %s\n", current->tapDevice);
- 			// strcpy((char *)current->tapDevice, tapDevice);
  			pthread_mutex_lock(&peer_mutex);
  			if (pthread_create(&connect_thread, NULL, connectToPeer, (void *)current) != 0) {
  				perror("connect_thread");
