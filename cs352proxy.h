1aa2ff5f8952c071de0da3dec5d5de5b5911a816
jracoma <jshinra@hotmail.com>
2013-12-02 00:17:13 -0500

    Working zero server and first client connect

--- cs352proxy.h
+++ cs352proxy.h
@@ -61,7 +61,7 @@ struct packetHeader {
 struct peerList {
   struct in_addr peerIP;
   uint16_t peerPort;
-  char tapDevice;
+  unsigned char tapDevice;
   struct timeval uniqueID;
   int net_fd;
   pthread_t pid;
