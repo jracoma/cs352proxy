32f80cc771cbd07c52c3606766cd961c3fc8ee17
jracoma <jshinra@hotmail.com>
2013-12-02 03:13:01 -0500

    Working zero server and first client connect

--- cs352proxy.c
+++ cs352proxy.c
@@ -416,7 +416,7 @@
  	sprintf(buffer, "0xabac %s %d %02x:%02x:%02x:%02x:%02x:%02x", inet_ntoa(ls->listenIP), ntohs(ls->listenPort), (unsigned char)ls->ethMAC.sa_data[0], (unsigned char)ls->ethMAC.sa_data[1], (unsigned char)ls->ethMAC.sa_data[2], (unsigned char)ls->ethMAC.sa_data[3], (unsigned char)ls->ethMAC.sa_data[4], (unsigned char)ls->ethMAC.sa_data[5]);
  	send(peer_fd, buffer, sizeof(buffer), 0);
  	printf("SENT2: %s\n", buffer);
- 	return buffer;
+ 	return &buffer;
  }
 
 /* Send linkStatePacket */
