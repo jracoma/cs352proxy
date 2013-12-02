2a08ee26b106c1a053cf0783f5162b8c47a4937b
jracoma <jshinra@hotmail.com>
2013-12-02 02:29:44 -0500

    Working zero server and first client connect

--- cs352proxy.c
+++ cs352proxy.c
@@ -410,6 +410,7 @@
  void send_linkState(struct linkState *ls, int peer_fd) {
  	char buffer[MAXBUFFSIZE];
 
+ 	/* Serialize Data - Packet Type | listenIP | listenPort | ethMAC */
  	sprintf(buffer, "0xabac %s %d %02x:%02x:%02x:%02x:%02x:%02x", inet_ntoa(ls->listenIP), ntohs(ls->listenPort), (unsigned char)ls->ethMAC.sa_data[0], (unsigned char)ls->ethMAC.sa_data[1], (unsigned char)ls->ethMAC.sa_data[2], (unsigned char)ls->ethMAC.sa_data[3], (unsigned char)ls->ethMAC.sa_data[4], (unsigned char)ls->ethMAC.sa_data[5]);
  	send(peer_fd, buffer, sizeof(buffer), 0);
  	printf("SENT2: %s\n", buffer);
@@ -428,13 +429,13 @@
  		}
  	}
 
- 	/* Serialize data */
- 	/* Packet Type | Packet Length | Source IP | Source Port | Eth MAC | Neighbors | UniqueID | linkWeight */
+ 	/* Serialize Data - Packet Type | Packet Length | Source IP | Source Port | Eth MAC | Neighbors | UniqueID | linkWeight */
  	lsp->header->length = sizeof(lsp);
  	sprintf(buffer, "0x%x %x %s %d %02x:%02x:%02x:%02x:%02x:%02x %d %ld:%ld %d", ntohs(lsp->header->type), lsp->header->length, inet_ntoa(lsp->source->ls->listenIP), ntohs(lsp->source->ls->listenPort), (unsigned char)lsp->source->ls->ethMAC.sa_data[0], (unsigned char)lsp->source->ls->ethMAC.sa_data[1], (unsigned char)lsp->source->ls->ethMAC.sa_data[2], (unsigned char)lsp->source->ls->ethMAC.sa_data[3], (unsigned char)lsp->source->ls->ethMAC.sa_data[4], (unsigned char)lsp->source->ls->ethMAC.sa_data[5], lsp->source->neighbors, lsp->uniqueID.tv_sec, lsp->uniqueID.tv_usec, lsp->linkWeight);
  	send(peer->net_fd, &buffer, sizeof(buffer), 0);
  	printf("SENT1: %s\n", buffer);
  	send_linkState(lsp->proxy1, peer->net_fd);
+ 	send_linkState(lsp->proxy2, peer->net_fd);
  	pthread_mutex_unlock(&peer_mutex);
  	pthread_mutex_unlock(&linkstate_mutex);
  }
