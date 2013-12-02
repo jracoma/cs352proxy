c6bdf5fef831f441ea7863bb69673a77961b8f82
jracoma <jshinra@hotmail.com>
2013-12-01 23:17:28 -0500

    Working zero server and first client connect

--- cs352proxy.c
+++ cs352proxy.c
@@ -416,7 +416,7 @@
  	/* Packet Type | Packet Length | Source IP | Source Port | Eth MAC | Neighbors | UniqueID | */
  	lsp->header->length = sizeof(lsp);
  	sprintf(buffer, "%x %x %s %d %02x:%02x:%02x:%02x:%02x:%02x %d %ld:%ld", lsp->header->type, lsp->header->length, inet_ntoa(lsp->source->ls->listenIP), ntohs(lsp->source->ls->listenPort), (unsigned char)lsp->source->ls->ethMAC.sa_data[0], (unsigned char)lsp->source->ls->ethMAC.sa_data[1], (unsigned char)lsp->source->ls->ethMAC.sa_data[2], (unsigned char)lsp->source->ls->ethMAC.sa_data[3], (unsigned char)lsp->source->ls->ethMAC.sa_data[4], (unsigned char)lsp->source->ls->ethMAC.sa_data[5], lsp->source->neighbors, lsp->uniqueID.tv_sec, lsp->uniqueID.tv_usec);
- 	send(peer->net_fd, &buffer, sizeof(buffer), 0);
+ 	send(peer->net_fd, &buffer, strlen(buffer), 0);
  	printf("SENT1: %s\n", buffer);
 
  	pthread_mutex_unlock(&peer_mutex);
