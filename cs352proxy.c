71295ab3d496673cecb9c27fbc8ed208d41ccfba
jracoma <jshinra@hotmail.com>
2013-12-10 20:31:06 -0500

    Working zero server and first client connect

--- cs352proxy.c
+++ cs352proxy.c
@@ -361,15 +361,14 @@
  	remote_addr.sin_port = htons(peer->lsInfo->listenPort);
  	inet_aton((char *)inet_ntoa(peer->lsInfo->listenIP), &remote_addr.sin_addr);
 
-
  	printf("NEW PEER: Connecting to %s:%d\n", inet_ntoa(remote_addr.sin_addr), ntohs(remote_addr.sin_port));
 
 /* Connect to server */
  	if ((connect(new_fd, (struct sockaddr *)&remote_addr, sizeof(remote_addr))) != 0) {
- 		printf("NEW PEER: Peer Removed %s:%d: Failed to connect\n", inet_ntoa(peer->lsInfo->listenIP), ntohs(remote_addr.sin_port));
+ 		printf("NEW PEER: Peer Removed %s:%d: Failed to connect\n", inet_ntoa(peer->lsInfo->listenIP), peer->lsInfo->listenPort);
  		pthread_exit(NULL);
  	} else {
- 		printf("NEW PEER: Connected to server %s:%d\n", inet_ntoa(remote_addr.sin_addr), ntohs(remote_addr.sin_port));
+ 		printf("NEW PEER: Connected to server %s:%d\n", inet_ntoa(remote_addr.sin_addr), peer->lsInfo->listenPort);
  	}
 /* Create link state packet */
  	gettimeofday(&current_time, NULL);
