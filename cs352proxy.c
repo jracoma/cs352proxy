6c059822d820f4c8769134501c0fb033856825d5
jracoma <jshinra@hotmail.com>
2013-12-10 20:35:56 -0500

    Working zero server and first client connect

--- cs352proxy.c
+++ cs352proxy.c
@@ -445,7 +445,7 @@
 /* Print peerList information */
  void print_peerList(struct peerList *peer) {
  	printf("---PEERLIST:\n");
- 	printf("NEW PEER: Connected to server %s:%d\n", inet_ntoa(peer->lsInfo->listenIP), peer->lsInfo->listenPort);
+ 	printf("NEW PEER89338: Connected to server %s:%d\n", inet_ntoa(peer->lsInfo->listenIP), peer->lsInfo->listenPort);
  	print_linkState(peer->lsInfo);
  	printf("----Tap: %s | UID: %ld:%ld | LinkWeight: %d | NET_FD: %d ", peer->tapDevice, peer->uniqueID.tv_sec, peer->uniqueID.tv_usec, peer->linkWeight, peer->net_fd);
  	if (peer->next == NULL) {
