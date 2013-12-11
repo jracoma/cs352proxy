cb6c423a5a1a8d5e385597a425dd2da78a226944
jracoma <jshinra@hotmail.com>
2013-12-11 01:22:40 -0500

    Working zero server and first client connect

--- cs352proxy.c
+++ cs352proxy.c
@@ -470,7 +470,7 @@
 
 /* Print linkState information */
  void print_linkState(struct linkState *ls) {
- 	char ethMac = malloc(sizeof(MAXBUFFSIZE));
+ 	char ethMAC[19];
  	sprintf(ethMAC, "%02x:%02x:%02x:%02x:%02x:%02x", (unsigned char)ls->ethMAC.sa_data[0], (unsigned char)ls->ethMAC.sa_data[1], (unsigned char)ls->ethMAC.sa_data[2], (unsigned char)ls->ethMAC.sa_data[3], (unsigned char)ls->ethMAC.sa_data[4], (unsigned char)ls->ethMAC.sa_data[5]);
  	printf("---LINKSTATE: listenIP: %s:%d | MAC: %s\n", inet_ntoa(ls->listenIP), ls->listenPort, ethMAC);
  }
