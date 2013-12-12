613619b5c8f3b090a97e9963dc3f4c928cb1391c
jracoma <jshinra@hotmail.com>
2013-12-11 21:34:49 -0500

    Working zero server and first client connect

--- cs352proxy.c
+++ cs352proxy.c
@@ -384,7 +384,7 @@
 /* Connect to server */
  	if ((connect(new_fd, (struct sockaddr *)&remote_addr, sizeof(remote_addr))) != 0) {
  		printf("NEW PEER: Peer Removed %s:%d: Failed to connect\n", inet_ntoa(peer->lsInfo->listenIP), peer->lsInfo->listenPort);
- 		// pthread_mutex_unlock(&peer_mutex);
+ 		pthread_mutex_unlock(&peer_mutex);
  		// return NULL;
  	} else {
  		printf("NEW PEER: Connected to server %s:%d\n", inet_ntoa(peer->lsInfo->listenIP), peer->lsInfo->listenPort);
