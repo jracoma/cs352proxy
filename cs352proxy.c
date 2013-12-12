0f7c13c76c2517765b5cdb6ca19f30f67f299963
jracoma <jshinra@hotmail.com>
2013-12-12 00:12:08 -0500

    Working zero server and first client connect

--- cs352proxy.c
+++ cs352proxy.c
@@ -509,7 +509,13 @@
  	new_peer->lsInfo->listenPort = atoi(strtok(NULL, " \n"));
  	next_field = strtok(NULL, " \n");
 	sscanf(next_field ,"%hhX:%hhX:%hhX:%hhX:%hhX:%hhX", (unsigned char *)&new_peer->lsInfo->ethMAC.sa_data[0], (unsigned char *)&new_peer->lsInfo->ethMAC.sa_data[1], (unsigned char *)&new_peer->lsInfo->ethMAC.sa_data[2], (unsigned char *)&new_peer->lsInfo->ethMAC.sa_data[3], (unsigned char *)&new_peer->lsInfo->ethMAC.sa_data[4], (unsigned char *)&new_peer->lsInfo->ethMAC.sa_data[5]);
-	// new_peer
+	next_field = atoi(strtok(NULL, " \n"))
+
+	if (!(next_field)) {
+		puts("SOLO!");
+	} else {
+		puts("NOT SOLO!");
+	}
 
 
 
