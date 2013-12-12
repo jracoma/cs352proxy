c30c03cfbc3cc33596e201865e5a650a7dd8497c
jracoma <jshinra@hotmail.com>
2013-12-11 22:35:04 -0500

    Working zero server and first client connect

--- cs352proxy.c
+++ cs352proxy.c
@@ -461,7 +461,7 @@
 /* Print peerList information */
  void print_peer(struct peerList *peer) {
  	print_linkState(peer->lsInfo);
- 	printf("----Tap: %s | UID: %ld:%ld | LinkWeight: %d | NET_FD: %d | ", peer->tapDevice, peer->uniqueID.tv_sec, peer->uniqueID.tv_usec, peer->linkWeight, peer->net_fd);
+ 	printf("----Tap: %s | UID: %ld:%ld | LinkWeight: %d | NET_FD: %d\n", peer->tapDevice, peer->uniqueID.tv_sec, peer->uniqueID.tv_usec, peer->linkWeight, peer->net_fd);
 
  	// if (peer->next == NULL) {
  	// 	printf("Next: NULL\n");
