3767ac1be45047b744b702d60ae38274c7ff650b
jracoma <jshinra@hotmail.com>
2013-12-12 17:04:04 -0500

    Working zero server and first client connect

--- cs352proxy.c
+++ cs352proxy.c
@@ -587,7 +587,7 @@
  	next_field = strtok(NULL, " \n");
  	strcpy(new_peer->tapDevice, next_field);
  	neighbors = atoi(strtok(NULL, " \n"));
- 	next_field = strtok(NULL, " \n");
+ 	next_field = strtok(NULL, "\n");
  	printf("REMAINING: %s", next_field);
  	printf("Neighbors: %d\n", neighbors);
  	if (!(neighbors)) {
