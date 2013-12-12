23fe551af4258512835cdf182ac69e94218e7fae
jracoma <jshinra@hotmail.com>
2013-12-11 21:47:52 -0500

    Working zero server and first client connect

--- cs352proxy.c
+++ cs352proxy.c
@@ -404,7 +404,7 @@
  		LL_COUNT(peerHead, tmp, lsPacket->neighbors);
  		send_singleLinkStatePacket(new_fd);
  		puts("NEW PEER: Single link state record sent.");
- 		if (debug) print_linkStatePacket(lsPacket);
+ 		if (debug) print_linkStatePacket();
 
  		// return NULL;
  	}
