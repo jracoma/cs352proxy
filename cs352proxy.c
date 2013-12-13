51b06863ce13e0362b84f5099f7f6876609236b1
jracoma <jshinra@hotmail.com>
2013-12-12 23:08:22 -0500

    Working zero server and first client connect

--- cs352proxy.c
+++ cs352proxy.c
@@ -418,7 +418,7 @@
  	puts("NEW PEER: Single link state record sent.");
  	sscanf(buffer ,"%hhX:%hhX:%hhX:%hhX:%hhX:%hhX %s", (unsigned char *)&peer->lsInfo->ethMAC.sa_data[0], (unsigned char *)&peer->lsInfo->ethMAC.sa_data[1], (unsigned char *)&peer->lsInfo->ethMAC.sa_data[2], (unsigned char *)&peer->lsInfo->ethMAC.sa_data[3], (unsigned char *)&peer->lsInfo->ethMAC.sa_data[4], (unsigned char *)&peer->lsInfo->ethMAC.sa_data[5], peer->tapDevice);
 
- 	printf("bah %d\n", add_member(peer));
+ 	if (add_member(peer)) puts("1");
 
  	print_peerList();
  	print_linkStateRecords();
