68195e8114274763ec299d190610740ef777970f
jracoma <jshinra@hotmail.com>
2013-12-13 04:08:24 -0500

    Working zero server and first client connect

--- cs352proxy.c
+++ cs352proxy.c
@@ -615,6 +615,7 @@
  	buf2 = send_peerList(record->proxy2);
  	if (debug) printf("TOTAL RECORDS: %d | ATTEMPTING TO ADD RECORD:\n%s - %d | %s - %d\n", HASH_COUNT(records), buf1, record->proxy1->net_fd, buf2, record->proxy2->net_fd);
 
+ 	puts("Checking proxy1 membership...");
  	if (!(record->proxy1) || add_peer(record->proxy1)) {
  		if (pthread_create(&connect_thread, NULL, connectToPeer, (void *)record->proxy1) != 0) {
  			perror("connect_thread");
@@ -622,6 +623,7 @@
  		}
  	}
 
+ 	puts("Checking proxy2 membership...");
  	if (!(record->proxy2) || add_peer(record->proxy2)) {
  		if (pthread_create(&connect_thread, NULL, connectToPeer, (void *)record->proxy2) != 0) {
  			perror("connect_thread");
