1c4635e5fcde6dbfffd18ccd1e7dc67b310c2d22
jracoma <jshinra@hotmail.com>
2013-12-14 13:28:19 -0500

    Working zero server and first client connect

--- cs352proxy.c
+++ cs352proxy.c
@@ -710,7 +710,7 @@
  		printf("Starting new thread for %s:%d\n", inet_ntoa(record->proxy2->listenIP), record->proxy2->listenPort);
  		if (pthread_create(&connect_thread, NULL, connectToPeer, (void *)record->proxy2) != 0) {
  			perror("connect_thread");
- 			pthread_mutex_unlock(&linkstate_mutex)
+ 			pthread_mutex_unlock(&linkstate_mutex);
  			pthread_exit(NULL);
  		}
  	}
