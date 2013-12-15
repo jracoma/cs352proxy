6f2796824b8301b9b03f6e53afacecf533801981
jracoma <jshinra@hotmail.com>
2013-12-14 21:58:33 -0500

    Working zero server and first client connect

--- cs352proxy.c
+++ cs352proxy.c
@@ -856,6 +856,7 @@
  	next_field = strtok(NULL, " \n");
  	strcpy(new_peer->tapDevice, next_field);
  	neighbors = atoi(strtok(NULL, " \n"));
+ 	next_field = strtok(NULL, " \n");
  	if (debug) printf("Neighbors: %d\n", neighbors);
  	if (!(neighbors)) {
  		sprintf(ethMAC, "%02x:%02x:%02x:%02x:%02x:%02x %s", (unsigned char)local_info->ethMAC.sa_data[0], (unsigned char)local_info->ethMAC.sa_data[1], (unsigned char)local_info->ethMAC.sa_data[2], (unsigned char)local_info->ethMAC.sa_data[3], (unsigned char)local_info->ethMAC.sa_data[4], (unsigned char)local_info->ethMAC.sa_data[5], dev);
@@ -865,7 +866,7 @@
  		decode_singleLinkStateRecord(next_field, in_fd);
  	} else {
  		if (debug) puts("NOT SINGLE!");
- 		next_field = strtok(NULL, "!");
+
  		// while ((next_field = strtok(NULL, "|"))) {
  			printf("NEXT: %s\n", next_field);
  		// }
